acbb2e7a84d2396063ef4fc9e736a74ccef8da13
Markus Binsteiner <makkus@posteo.de>
2017-05-05 10:24:05 +0200

    Python 3 compatibility stuff

--- frkl/frkl.py
+++ frkl/frkl.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# python 3 compatibility
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__metaclass__ = type
+
 log = logging.getLogger("freckles")
 
 DEFAULT_ABBREVIATIONS = {
