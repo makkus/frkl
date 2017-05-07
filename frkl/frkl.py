65e1b734ca1ea4fddd7da807b830ebe6d9d16d79
Markus Binsteiner <makkus@posteo.de>
2017-05-07 14:39:27 +0200

    Initial work on preprocessing, and some tests.

--- frkl/frkl.py
+++ frkl/frkl.py
@@ -3,21 +3,255 @@
 # python 3 compatibility
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import copy
 import logging
-
+import pprint
+import re
+import os
 
 __metaclass__ = type
 
 log = logging.getLogger("freckles")
 
+PLACEHOLDER = -9876
+
 DEFAULT_ABBREVIATIONS = {
-    'gh': ["https://raw.githubusercontent.com", -1, -1, "master"]
+    'gh': ["https://raw.githubusercontent.com", PLACEHOLDER, PLACEHOLDER, "master"],
+    'bb': ["https://bitbucket.org", PLACEHOLDER, PLACEHOLDER, "src", "master"]
 }
 
 
+def dict_merge(dct, merge_dct, copy_dct=True):
+    """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+
+    Copied from: https://gist.github.com/angstwad/bf22d1822c38a92ec0a9
+
+    Args:
+      dct (dict): dict onto which the merge is executed
+      merge_dct (dict): dct merged into dct
+    Kwargs:
+      copy_dct (bool): whether to (deep-)copy dct before merging (and leaving it unchanged), or not (default: copy)
+
+    Returns:
+      dict: the merged dict (original or copied)
+    """
+
+    if copy_dct:
+        dct = copy.deepcopy(dct)
+
+    for k, v in merge_dct.items():
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.Mapping)):
+            dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
+
+    return dct
+
+
+
+class ConfigProcessor(object):
+    """Abstract base class for config url/content manipulators.
+
+    In order to enable configuration urls and content to be written as quickly and minimal as possible, frkl supports pluggable processors that can manipulate the configuration urls and contents. For example, urls can be appbreviated 'gh' -> 'https://raw.githubusercontent.com/blahblah'.
+    """
+
+    def process(self, input_config):
+        """Processes the config url or content.
+
+        Args:
+          input_config (str): input configuration url or content
+
+        Returns:
+          str: processed config url or content
+        """
+
+        return None
+
+class RegexProcessor(ConfigProcessor):
+
+    def __init__(self, regexes):
+        """Replaces all occurences of regex matches.
+
+        Args:
+          regexes (dict): a map of regexes and their replacements
+        """
+
+        self.regexes = regexes
+
+    def process(self, input_config):
+
+        new_config = input_config
+
+        for regex, replacement in self.regexes.items():
+            new_config = re.sub(regex, replacement, new_config)
+
+        return new_config
+
+class UrlAbbrevProcessor(ConfigProcessor):
+
+    def __init__(self, abbrevs={}, add_default_abbrevs=True):
+        """Replaces strings in an input configuration url with its expanded version.
+
+        The default constructor without any arguments will create a processor only using the default, inbuilt abbreviations
+
+        Kwargs:
+          abbrevs (dict): custom abbreviations to use
+          add_default_abbrevs (bool): whether to add the default abbreviations
+        """
+
+        if not abbrevs:
+            if add_default_abbrevs:
+                self.abbrevs = DEFAULT_ABBREVIATIONS
+            else:
+                self.abbrevs = {}
+        else:
+            if add_default_abbrevs:
+                self.abbrevs = copy.deepcopy(DEFAULT_ABBREVIATIONS)
+                dict_merge(self.abbrevs, abbrevs, copy_dct=False)
+            else:
+                self.abbrevs = copy.deepcopy(abbrevs)
+
+    def process(self, input_config):
+        return self.expand_config(input_config)
+
+    def expand_config(self, config):
+        """Expands abbreviated configuration urls that start with `<token>:`.
+
+        This is a convenience for the user, as they don't have to type out long urls if they don't want to.
+
+        To make it easier to remember (and shorter to type) config urls, *frkl*
+        supports abbreviations which will be replaced before attempting to load a
+        configuration. In addition to inbuild abbreviations, a custom ones can be
+        piped into the Frkl constructor. This needs to be a dictionary of string to
+        string (abbreviation -> full url-part, eg. ``freckles_configs -->
+        https://raw.githubusercontent.com/makkus/freckles/master/examples``, which
+        would enable the user to provide this config url:
+        ``freckles_config:quickstart.yml``) or string to list (abbreviation -> list
+        of tokens to assemble the finished string, which for example for getting
+        raw files from the master branch of a github repo would look like:
+        ["https://raw.githubusercontent.com", frkl.PLACEHOLDER, frkl.PLACEHOLDER,
+        "master] -- the input config ``gh:makkus/freckles/examples/quickstart.yml``
+        would result in the same string as in the first example above -- this
+        method is a bit more fragile, because the input string can't have less
+        tokens seperated by '/' than the value list).
+
+        Args:
+          config (str): the configuration url/json/etc...
+
+        Returns:
+          str: the configuration with all occurances of registered abbreviations replaced
+
+        """
+
+        prefix, sep, rest = config.partition(':')
+
+
+        if prefix in self.abbrevs.keys():
+
+            if isinstance(self.abbrevs[prefix], basestring):
+                return "{}{}".format(self.abbrevs[prefix], rest)
+            else:
+                tokens = rest.split("/")
+                result_string = ""
+                for t in self.abbrevs[prefix]:
+
+                    if t == PLACEHOLDER:
+                        if not tokens:
+                            raise FrecklesConfigError("Can't expand url '{}': not enough parts.", 'config', url)
+                        to_append = tokens.pop(0)
+                    else:
+                        to_append = t
+
+                    result_string += to_append
+                    result_string += "/"
+
+                if tokens:
+                    postfix = "/".join(tokens)
+                    result_string += postfix
+
+                    return result_string
+        else:
+            return config
+
+
+DEFAULT_PROCESSOR_CHAIN = [UrlAbbrevProcessor()]
+
+def process_chain(configs, processor_chain):
+    """Processes the given input using the proviced chain of processors.
+    """
+
+    result = []
+
+    for c in configs:
+        new_config = c
+
+        for p in processor_chain:
+            new_config = p.process(new_config)
+        result.append(new_config)
+
+    return result
+
 class Frkl(object):
 
-    """Base object to hold the configuration itself."""
+    def __init__(self, configs, stem_key="config", other_valid_keys=[], processor_chain=DEFAULT_PROCESSOR_CHAIN):
+        """Base object that holds the configuration.
 
-    def __init__(object):
-        """"""
+        Args:
+          configs (list): list of configurations, will be processed in the order they come in
+        Kwargs:
+          stem_key (str): the key that is used to travers a level up in the configuration, generating 'child' configs
+          other_valid_keys (list): list of keynames that are valid config 'holders'
+          processor_chain (list): processor chain to use, defaults to [:class:`UrlAbbrevProcessor`]
+
+        Returns:
+          str: the final config url, all abbreviations replaced
+        """
+
+        self.orig_configs = configs
+        self.stem_key = stem_key
+        self.other_valid_keys = other_valid_keys
+        self.processor_chain = processor_chain
+
+        self.configs = process_chain(self.orig_configs, self.processor_chain)
+
+
+
+    def get_config(config):
+        """Retrieves the configuration, including pre-processing.
+
+        Args:
+          config (str): the configuration url/json/etc...
+
+        Returns:
+          dict: the pre-processed configuration dict
+        """
+
+        if isinstance(config, dict):
+            log.debug("Not processing config, already dict: {}".format(config))
+            return config
+
+        # expanding configuration url if applicable
+
+
+        if os.path.exists(config):
+            log.debug("Opening as file: {}".format(config))
+            with open(config_file_url) as f:
+                content = f.read()
+
+        
+
+    def frklize_config(self, root, configs):
+        """Read all configs in order, generate a composite.
+
+        Args:
+          root (list): root list to add new configurations onto
+          configs (list): configurations to add
+        """
+
+        for c in configs:
+
+            self.get_config(c)
--- tests/test_frkl.py
+++ tests/test_frkl.py
@@ -12,29 +12,99 @@ Tests for `frkl` module.
 import sys
 import unittest
 from contextlib import contextmanager
-from click.testing import CliRunner
+#from click.testing import CliRunner
+
+import pytest
+
+import os
+import yaml
+
+import copy
 
 from frkl import frkl
 from frkl import cli
 
+TEST_DICTS = [
+    ({}, {}, {}),
+    ({'a': 1}, {'a': 1}, {'a': 1}),
+    ({'a': 1}, {'b': 1}, {'a': 1, 'b':1} ),
+    ({'a': 1}, {'a': 2}, {'a': 2}),
+    ({'a': 1, 'aa': 11}, {'b': 2, 'bb': 22}, {'a': 1, 'aa': 11, 'b': 2, 'bb': 22}),
+    ({'a': 1, 'aa': 11}, {'b': 2, 'aa': 22}, {'a': 1, 'b': 2, 'aa': 22})
+]
+
+TEST_CUSTOM_ABBREVS = {
+    "test_abbr1": "https://example.url/folder1/folder2/"
+}
+
+TEST_ABBREV_URLS = [
+    ("gh:makkus/freckles/examples/quickstart.yml", "https://raw.githubusercontent.com/makkus/freckles/master/examples/quickstart.yml"),
+    ("bb:makkus/freckles/examples/quickstart.yml", "https://bitbucket.org/makkus/freckles/src/master/examples/quickstart.yml"),
+    ("test_abbr1:file1", "https://example.url/folder1/folder2/file1")
+]
+
+TEST_REGEXES = {
+    "^start": "replacement",
+    "frkl_expl": "makkus/freckles/examples"
+}
+
+TEST_REGEX_URLS = [
+    ("start_resturl", "replacement_resturl"),
+    ("xstart_resturl", "xstart_resturl"),
+    ("begin/frkl_expl/end", "begin/makkus/freckles/examples/end"),
+    ("start/frkl_expl/end", "replacement/makkus/freckles/examples/end")
+]
+
+TEST_PROCESSOR_CHAIN = [frkl.RegexProcessor(TEST_REGEXES), frkl.UrlAbbrevProcessor(TEST_CUSTOM_ABBREVS)]
+
+TEST_CHAIN_URLS = [
+    (["gh:makkus/freckles/examples/quickstart.yml"], ["https://raw.githubusercontent.com/makkus/freckles/master/examples/quickstart.yml"]),
+    (["bb:makkus/freckles/examples/quickstart.yml"], ["https://bitbucket.org/makkus/freckles/src/master/examples/quickstart.yml"]),
+    (["gh:frkl_expl/quickstart.yml"], ["https://raw.githubusercontent.com/makkus/freckles/master/examples/quickstart.yml"])
+]
+
+@pytest.mark.parametrize("dict1, dict2, expected", TEST_DICTS)
+def test_dict_merge_copy_result(dict1, dict2, expected):
+
+    dict1_orig = copy.deepcopy(dict1)
+    dict2_orig = copy.deepcopy(dict2)
+    merged = frkl.dict_merge(dict1, dict2, True)
+    assert merged == expected
+    assert dict1 == dict1_orig
+    assert dict2 == dict2_orig
+
+@pytest.mark.parametrize("dict1, dict2, expected", TEST_DICTS)
+def test_dict_merge_dont_copy_result(dict1, dict2, expected):
+
+    dict1_orig = copy.deepcopy(dict1)
+    dict2_orig = copy.deepcopy(dict2)
+    merged = frkl.dict_merge(dict1, dict2, False)
+    assert merged == expected
+    assert dict1 == expected
+    assert dict2 == dict2_orig
+
+@pytest.mark.parametrize("input_url, expected", TEST_ABBREV_URLS)
+def test_expand_config(input_url, expected):
+
+    prc = frkl.UrlAbbrevProcessor(abbrevs=TEST_CUSTOM_ABBREVS)
+    result = prc.process(input_url)
+
+    assert result == expected
+
+@pytest.mark.parametrize("input_url, expected", TEST_REGEX_URLS)
+def test_regex_processor(input_url, expected):
+
+    prc = frkl.RegexProcessor(TEST_REGEXES)
+    result = prc.process(input_url)
+
+    assert result == expected
 
 
-class TestFrkl(unittest.TestCase):
 
-    def setUp(self):
-        pass
+@pytest.mark.parametrize("input_urls, expected", TEST_CHAIN_URLS)
+def test_config_chain_processors(input_urls, expected):
 
-    def tearDown(self):
-        pass
+    f = frkl.Frkl(input_urls, processor_chain=TEST_PROCESSOR_CHAIN)
 
-    def test_000_something(self):
-        pass
+    assert f.configs == expected
 
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'frkl.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
