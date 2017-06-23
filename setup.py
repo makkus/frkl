8cb5a1fdff069dfbc7b1dfcb485c957fa82fff88
Markus Binsteiner <makkus@posteo.de>
2017-06-23 21:09:14 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='frkl',
-    version='0.1.0',
+    version='0.1.1',
     description="Elastic configuration files",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
