6d8fc6abf244ad009419d95912eda9709e51bccc
Markus Binsteiner <makkus@posteo.de>
2018-05-30 19:40:57 +0200

    Adding get_url_parents helper method.

--- frkl/utils.py
+++ frkl/utils.py
@@ -4,11 +4,12 @@ import logging
 
 from six import string_types
 
+from frutils.defaults import *
 from .frkl import Frkl
-from .processors import UrlAbbrevProcessor
+from .processors import UrlAbbrevProcessor, EnsureUrlProcessor, ParentPathProcessor
+from .callbacks import SetResultCallback
 
 log = logging.getLogger("frkl")
-
 
 def expand_string_to_git_details(value, default_abbrevs):
 
@@ -67,3 +68,27 @@ def expand_string_to_git_repo(value, default_abbrevs):
             return result
     except (Exception) as e:
         raise Exception("'{}' is not a valid repo url: {}".format(value, e))
+
+
+def get_url_parents(urls, abbrevs=False, return_list=False):
+    """Helper methods to calculate the parents of the provided urls.
+
+    Args:
+        urls (list): the list of urls
+        abbrevs (bool, dict): if False, urls won't be expanded if they are abbreviated, otherwise if a abbrev dict is provided, they will be
+        return_list (bool): whether to return the result as set (False) or list (True)
+    """
+
+    if abbrevs is False or abbrevs is None:
+        chain = [ParentPathProcessor()]
+    else:
+        if abbrevs is True:
+            abbrevs = DEFAULT_URL_ABBREVIATIONS_FILE
+        chain = [UrlAbbrevProcessor(init_params={"abbrevs": abbrevs, "add_default_abbrevs": False}), ParentPathProcessor()]
+    callback = SetResultCallback(init_params={"return_list": return_list})
+
+    frkl_obj = Frkl(urls, chain)
+    result = frkl_obj.process(callback)
+
+    return result
+
