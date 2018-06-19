44557e9ffd30121f009641257a625439ef6bfe38
Markus Binsteiner <makkus@posteo.de>
2018-06-19 23:32:22 +0200

    Re-add module-level imports.

--- frkl/__init__.py
+++ frkl/__init__.py
@@ -3,3 +3,18 @@
 __author__ = """Markus Binsteiner"""
 __email__ = "makkus@posteo.de"
 __version__ = "0.3.0"
+
+from .frkl import (
+    Frkl,
+    load_object_from_url_or_path,
+    load_string_from_url_or_path,
+    load_templated_string_from_url_chain,
+)
+from .processors import (
+    UrlAbbrevProcessor,
+    EnsurePythonObjectProcessor,
+    EnsureUrlProcessor,
+    LoadMoreConfigsProcessor,
+    FrklProcessor,
+)
+from .helpers import get_full_url, download_cached_file, content_from_url, dict_from_url
