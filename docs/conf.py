1cc88fe6522b06936c9a8268ccb394e615c623a7
Markus Binsteiner <makkus@posteo.de>
2018-06-20 12:04:32 +0200

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ import sys
 import frkl
 import sphinx_redactor_theme
 
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath("../"))
 
 # -- General configuration ---------------------------------------------
